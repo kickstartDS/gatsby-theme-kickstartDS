d43fac751a42e333df02ea6d92995bf6d4be2d19
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-02-20 22:56:06 +0100

    feat(wordpress): use wordpress source

---
+++ example/src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const NotFound = () => (
+  <Layout>
+    <div>
+      <h1>Oops, that's a 404</h1>
+    </div>
+  </Layout>
+)
+
+export default NotFound;
