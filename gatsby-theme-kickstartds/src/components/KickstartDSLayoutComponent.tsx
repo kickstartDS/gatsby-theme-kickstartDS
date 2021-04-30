5f76b60af45a8323a8338a440d4ffdd5452f447b
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-04-30 21:27:10 +0200

    fix(cleanup): remove hard-coded css

--- gatsby-theme-kickstartds/src/components/KickstartDSLayoutComponent.tsx
+++ gatsby-theme-kickstartds/src/components/KickstartDSLayoutComponent.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from 'react';
 
 import "@kickstartds/base/lib/global/base.css";
 import "@kickstartds/core/lib/design-tokens/tokens.css";
+import "../assets/css/tokens.css";
 
 // TODO add `KickstertLayoutProps`
 export const KickstartDSLayout: FunctionComponent<KickstartLayoutProps> = ({ children }) => (
