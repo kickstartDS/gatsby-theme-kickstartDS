99c7eb49a53cd6a6168c1bf63421e040a56f7669
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-06-03 12:52:42 +0200

    feat: add fragments, query fragments

--- gatsby-theme-kickstartds/create/createPages.js
+++ gatsby-theme-kickstartds/create/createPages.js
@@ -6,65 +6,10 @@ module.exports = async ({ actions, graphql }) => {
       allKickstartDsPage {
         edges {
           node {
-            id
-            title
-            heading
             content {
-              background
-              gutter
-              mode
-              spaceAfter
-              spaceBefore
-              space_after
-              space_before
-              type
-              width
-              headline {
-                align
-                content
-                level
-                pageHeader
-                spaceAfter
-                subheadline
-              }
               content {
-                ... on ContentBoxSchema {
-                  topic
-                  alignement
-                  type
-                  text
-                  ratio
-                  image
-                }
-                ... on CountUpSchema {
-                  topic
-                  text
-                  to
-                  link {
-                    className
-                    dataComponent
-                    fillAnimation
-                    href
-                    iconAfter
-                    iconAnimation
-                    iconBefore
-                    label
-                    newTab
-                    variant
-                    size
-                    icon {
-                      className
-                      icon
-                      role
-                    }
-                  }
-                  icon {
-                    className
-                    icon
-                    role
-                  }
-                }
-                text
+                ...CountUpComponentDeepNesting
+                ...ContentBoxComponentDeepNesting
               }
             }
           }
