52af4667609df313ee368164af1ae8089d15b495
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-06-06 23:10:26 +0200

    chore: remove unneeded comment

--- gatsby-theme-kickstartds/src/templates/page.js
+++ gatsby-theme-kickstartds/src/templates/page.js
@@ -1,6 +1,5 @@
 import { KickstartDSPage } from "../components/KickstartDSPageComponent";
 
-// TODO: convert to .ts / .tsx
 export const KickstartDSList = (props) => {
   const page = props.pageContext.page;
 
