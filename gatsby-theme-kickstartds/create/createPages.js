b5854c76d1388129f0d143b7f0060fd4c7b91d61
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-06-13 14:16:09 +0200

    feat: be intelligent about collecting fragments

--- gatsby-theme-kickstartds/create/createPages.js
+++ gatsby-theme-kickstartds/create/createPages.js
@@ -6,26 +6,49 @@ const GatsbyParser = require('gatsby/dist/query/file-parser').default;
  * Collect all graphql fragments from a directory
  * @see https://github.com/gatsbyjs/gatsby/issues/12155#issuecomment-618424527
  */
-const collectGraphQLFragments = async (fragmentsNamesToExtract) => {
+const collectGraphQLFragments = async (fragmentNamesToExtract) => {
   const parser = new GatsbyParser();
   const result = await parser.parseFile(path.resolve(__dirname, '../fragments.js'));
 
-  return result
-    .filter((item) => item.doc && item.doc.kind === "Document")
-    .flatMap((file) => {
-      const fragments =
-        file.doc.definitions.filter(
-          (def) => def.kind === "FragmentDefinition"
-        ) || [];
+  const collectFromSpreads = (fragment) => fragment.selectionSet.selections
+    .flatMap((item) => item.selectionSet ? item.selectionSet.selections : [])
+    .filter((item) => item.kind === 'FragmentSpread')
+    .map((spread) => spread.name.value);
 
-      return fragments
-        .filter((fragment) =>
-          fragmentsNamesToExtract.includes(fragment.name.value)
-        )
-        .map(({ loc: { start, end, source: { body } } }) =>
-          body.slice(start, end)
-        );
-    })
+  const getNestedFragments = (fragment, allFragments) => {
+    const collectedFragmentNames = collectFromSpreads(fragment);
+    const collectedFragments = allFragments.filter((fragment) => {
+      return collectedFragmentNames.includes(fragment.name.value);
+    });
+
+    if (collectedFragments.length === 0) {
+      return [];
+    } else {
+      return collectedFragments.concat(
+        collectedFragments.flatMap((collectedFragment) => getNestedFragments(collectedFragment, allFragments))
+      );
+    }
+  };
+
+  const allFragments = result
+    .filter((item) => item.doc && item.doc.kind === "Document")
+    .flatMap((document) => document.doc.definitions.filter(
+      (def) => def.kind === "FragmentDefinition"
+    ) || []);
+
+  const requiredFragments = allFragments
+    .filter((fragment) =>
+      fragmentNamesToExtract.includes(fragment.name.value)
+    );
+
+  const nestedFragments = requiredFragments
+    .flatMap((requiredFragment) => getNestedFragments(requiredFragment, allFragments));
+
+  return [...requiredFragments, ...nestedFragments]
+    .filter((value, index, self) => self.map(x => x.name.value).indexOf(value.name.value) == index)
+    .map(({ loc: { start, end, source: { body } } }) =>
+      body.slice(start, end)
+    )
     .join("\n");
 };
 
