699f65d2b4c21aaa3685cada154e1ec348410e46
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-09-28 16:49:38 +0200

    build: create static components map for loadable components

---
+++ gatsby-theme-kickstartds/create/createComponentsMap.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path');
+const baseExports = require('@kickstartds/base/lib/exports.json');
+const blogExports = require('@kickstartds/blog/lib/exports.json');
+const contentExports = require('@kickstartds/content/lib/exports.json');
+
+const reducer = (mod) => (prev, [key, value]) => {
+  if (key !== "index.js" && key.indexOf('/') === -1 && value.length) {
+    prev += `'${key}': loadable(() => import('@kickstartds/${mod}/lib/${key}/index.js'), { resolveComponent: (exports) => exports.${value[0]} }),\n`
+  }
+  return prev
+};
+
+const base = Object.entries(baseExports).reduce(reducer("base"), '');
+const blog = Object.entries(blogExports).reduce(reducer("blog"), '');
+const content = Object.entries(contentExports).reduce(reducer("content"), '');
+
+const dest = path.resolve(__dirname, '../src/components', 'kickstartDSComponentsMap.ts');
+const file = `
+import loadable from '@loadable/component';
+
+export default {
+  ${base}
+  ${blog}
+  ${content}
+};
+`;
+fs.writeFileSync(dest, file);
