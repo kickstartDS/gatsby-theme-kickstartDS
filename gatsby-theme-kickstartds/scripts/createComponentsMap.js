6a1af34a2714ed72d51e431fa43ff70c9f9dc956
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-10-31 22:02:17 +0100

    fix: final fix... probably ;)

--- gatsby-theme-kickstartds/scripts/createComponentsMap.js
+++ gatsby-theme-kickstartds/scripts/createComponentsMap.js
@@ -25,7 +25,7 @@ const pathPrefix = process.cwd().includes('ramdisk')
   ? '/mnt/ramdisk/'
   : fs.existsSync('../dist')
     ? '../'
-    : '';
+    : '../../../';
 const exportJsons = fg.sync(`${pathPrefix}node_modules/**/dist/exports.json`);
 const additionalPackage = exportJsons[0].match(/node_modules\/(.*)\/dist\/exports\.json/)[1];
 const additionalExports = exportJsons.map((exportJson) => {
