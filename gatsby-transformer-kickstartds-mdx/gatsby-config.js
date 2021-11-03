d35d28895b0e5eab0004e79328b5b27661a0895f
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-11-03 22:58:39 +0100

    fix: initial draft for mdx support

--- gatsby-transformer-kickstartds-mdx/gatsby-config.js
+++ gatsby-transformer-kickstartds-mdx/gatsby-config.js
@@ -2,12 +2,13 @@ module.exports = {
   plugins: [{
     resolve: `gatsby-plugin-mdx`,
     options: {
-      gatsbyRemarkPlugins: [{
-        resolve: "gatsby-remark-shiki-twoslash",
-          options: {
-            theme: "nord",
-          }
-        },
+      gatsbyRemarkPlugins: [
+        // {
+        //   resolve: "gatsby-remark-shiki-twoslash",
+        //   options: {
+        //     theme: "nord",
+        //   }
+        // },
       ],
     },
   }],
