6be531e5dabcfef087fc7d1babc18c9bd05b26ab
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-01-23 18:46:57 +0100

    feature: start adding fsMap with needed types
    still only hardcodes glossary types, needs to be done dynamically, maybe
    utilizing our exports.json to inform what to include

--- gatsby-transformer-kickstartds-mdx/gatsby-config.js
+++ gatsby-transformer-kickstartds-mdx/gatsby-config.js
@@ -1,3 +1,17 @@
+import { createDefaultMapFromNodeModules } from "@typescript/vfs";
+import { fs } from 'fs';
+
+const fsMap = createDefaultMapFromNodeModules({});
+
+const reactDts = fs.readFileSync(require.resolve('/node_modules/@types/react/index.d.ts'), 'utf-8');
+const kdsDts = fs.readFileSync(require.resolve('/node_modules/@kickstartds/design-system/dist/components/types.d.ts'), 'utf-8');
+
+console.log(reactDts);
+console.log(kdsDts);
+
+fsMap.set("react", reactDts);
+fsMap.set("/node_modules/@kickstartds/design-system/dist/components/types.d.ts", kdsDts);
+
 module.exports = {
   plugins: [{
     resolve: `gatsby-plugin-mdx`,
@@ -7,6 +21,7 @@ module.exports = {
           resolve: require.resolve("gatsby-remark-shiki-twoslash"),
           options: {
             theme: "material-default",
+            fsMap,
           },
         },
         `gatsby-remark-autolink-headers`,
