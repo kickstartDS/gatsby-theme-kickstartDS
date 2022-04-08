94f962f7ff9415c42232d870f13759e45b6747c4
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-04-08 22:17:44 +0200

    feature: add first stab at component telemetry
    does nothing so far

--- gatsby-theme-kickstartds/src/helpers/componentMapper.tsx
+++ gatsby-theme-kickstartds/src/helpers/componentMapper.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
-import components from "../components/ComponentsMap";
+import { InfluxDB, Point } from '@influxdata/influxdb-client-browser';
 
 import { cleanObjectKeys } from '@kickstartds/jsonschema2graphql/build/dehashing';
+
+import components from "../components/ComponentsMap";
+
+const url = process.env['INFLUX_URL'] || 'http://localhost:8086';
+const token = process.env['INFLUX_TOKEN'] || 'my-token';
+const org = process.env['INFLUX_ORG'] || 'my-org';
+const bucket = process.env['INFLUX_BUCKET'] || 'my-bucket';
+
+const writeApi = new InfluxDB({url, token}).getWriteApi(org, bucket, 'ns');
+writeApi.useDefaultTags({location: 'calculon01'});
+
+const componentStatistics = (componentName: string, componentProps: Record<string, any>) => {
+  const componentStats = new Point(componentName)
+    .tag(componentName, 'component_stats')
+    .intField('value', componentProps.length || 0)
+    .timestamp(new Date());
+  writeApi.writePoint(componentStats);
+  console.log(` ${componentStats.toLineProtocol(writeApi)}`);
+  console.log('TEST');
+  writeApi.flush();
+};
 
 export const componentCounter = [];
 export const getComponent = (element, isSection = false) => {
@@ -29,6 +50,9 @@ export const getComponent = (element, isSection = false) => {
 
   const cleanedElement: Record<string, any> = cleanObjectKeys(element);
   const { typeProp, type, ...restElement } = cleanedElement;
+
+  componentStatistics(typeProp, cleanedElement);
+  
   return <Component type={typeProp} key={key} { ...restElement } />;
 };
 
