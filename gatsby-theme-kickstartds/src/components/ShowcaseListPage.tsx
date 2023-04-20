afb0412a7fbecf78b4074d55c87194e04bf1dcb9
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2023-04-20 16:34:17 +0200

    fix: empty appearances objects

--- gatsby-theme-kickstartds/src/components/ShowcaseListPage.tsx
+++ gatsby-theme-kickstartds/src/components/ShowcaseListPage.tsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import { FunctionComponent } from 'react';
+import React from "react";
+import { FunctionComponent } from "react";
 
 import { Section } from "@kickstartds/base/lib/section";
 import { PostTeaser } from "@kickstartds/blog/lib/post-teaser";
 
-import { Layout } from './Layout';
+import { Layout } from "./Layout";
 
 export const ShowcaseListPage: FunctionComponent<any> = ({
-  postTeaser, ...rest
-}) =>
+  showcaseTeaser,
+  ...rest
+}) => (
   <Layout {...rest}>
     <Section
       headline={{
         content: "Showcases – projects built on kickstartDS",
         level: "h1",
-        subheadline:
-          "This includes Design Systems, websites, apps, etc.",
+        subheadline: "This includes Design Systems, websites, apps, etc.",
         align: "center",
       }}
       width="wide"
       spaceAfter="none"
     />
 
-    {postTeaser && postTeaser.length > 0 && (
-      <Section
-        spaceBefore="default"
-        width="narrow"
-        mode="list"
-        gutter="large">
-        {postTeaser.map((teaser) => <PostTeaser {...teaser} />)}
+    {showcaseTeaser && showcaseTeaser.length > 0 && (
+      <Section spaceBefore="default" width="narrow" mode="list" gutter="large">
+        {showcaseTeaser.map((teaser) => (
+          <PostTeaser {...teaser} />
+        ))}
       </Section>
     )}
-  </Layout>;
+  </Layout>
+);
